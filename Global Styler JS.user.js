3ea3e699ce879250619f9d2ba23d2100f8c7703a
SyndiShanX <brianmatthew.1026@gmail.com>
2024-06-23 07:27:03 -0400

    Fixed Custom Nav Menu

--- Global Styler JS.user.js
+++ Global Styler JS.user.js
@@ -20,6 +20,7 @@
 
 // Updated (8/19/20) synMenu3 was broken server-side
 // Updated (12/1/20) synMenu3 was fixed server-side --synHTML3Fix = synHTML3[0].split("</a></li>\n\n<li class=")--
+// Updated (6/23/24) Tools Section was moved to the Left Sidemenu
 // Left Menu
 synMenuEnd = "</a></li></ul></div>"
 synMenuStart = '<li class='
@@ -27,13 +28,15 @@ synHTML1 = document.getElementsByClassName("leftmenu")[0].outerHTML
 synHTML2 = synHTML1.split("<ul>")
 synHTML3 = synHTML2[1].split("</a></li>\n<li class=")
 
+synHTML8 = synHTML1.split("Tools")[1].split('\n<li class')[0]
+
 synLeftMenu1 = '<div class="menu1"><ul>' + synHTML3[0] + synMenuEnd
 synLeftMenu1 = synLeftMenu1.split("\n\n")
 synLeftMenu1 = synLeftMenu1[0] + "\n" + synLeftMenu1[1]
 synLeftMenu2 = '<div class="menu2"><ul>' + synMenuStart + synHTML3[1] + synMenuEnd
 synLeftMenu3 = '<div class="menu3"><ul>' + synMenuStart + synHTML3[2] + synMenuEnd
 synLeftMenu3 = synLeftMenu3.split("Dep. Store</a></li>")[0] + 'Dep. Store</a></li>\n<li><a href="sell.php"' + synLeftMenu3.split('<li><a href="sell.php"')[1]
-synLeftMenu4 = '<div class="menu6"><ul>' + synMenuStart + synHTML3[3] + synMenuEnd
+synLeftMenu4 = '<div class="menu6"><ul>' + synMenuStart + synHTML3[4] + synMenuEnd
 synLeftMenu = synLeftMenu1 + "\n\n" + synLeftMenu2 + "\n\n" + synLeftMenu3 + "\n\n" + synLeftMenu4
 
 // Right Menu
@@ -42,18 +45,17 @@ synHTML5 = synHTML4.split('<li class="header">')
 synHTML6 = synHTML5[3] + '<li class="header">' + synHTML5[4] + '<li class="header">' + synHTML5[5] + '<li class="header">' + synHTML5[6] + '<li class="header">' + synHTML5[7]
 synHTML6 = synHTML6.replace('\n\n', '\n')
 synHTML7 = synHTML6.split("</a></li>\n<li class=")
-synHTML8 = synHTML7[3].split("Tools")
-synHTML9 = synHTML3[4].split("Account</li>")
+synHTML9 = synHTML3[5].split("Account</li>")
 
 synRightMenu1 = '<div class="menu5"><ul><li class="header">' + synHTML7[0] + synMenuEnd
 synRightMenu2 = '<div class="menu4"><ul>' + synMenuStart + synHTML7[2] + synMenuEnd
 synRightMenu2 = synRightMenu2.split(" Rankings")
 synRightMenu2 = synRightMenu2.join("")
-synRightMenu3 = '<div class="menu7"><ul>' + synMenuStart + synHTML8[0] + "Other" + synHTML8[1] + "</a></li>" + synHTML7[4].split('"header">Other</li>')[1].split("</ul>")[0] + "</ul></div>"
+synRightMenu3 = '<div class="menu7"><ul>' + synMenuStart + '"header">Other' + synHTML8 + "</a></li>" + synHTML7[3].split('"header">Other</li>')[1].split("</ul>")[0] + "</ul></div>"
 synRightMenu4 = synRightMenu3.split('Staff List</a></li>\n')[1].split('\n')[0]
-synRightMenu3 = synRightMenu3.split('Staff List</a></li>\n')[0] + 'Staff List</a></li>\n' + synRightMenu3.split('Staff List</a></li>\n')[1].split('My Profile</a></li>\n')[1]
-synRightMenu5 = '<div class="menu8"><ul>' + synMenuStart + synHTML9[0] + "Account/Clan</li>" + synHTML7[1].split('"header">Your Clan</li>')[1] + synHTML9[1]
-synRightMenu5 = synRightMenu5.split('</a></li>\n</ul>')[0] + '</a></li>' + synRightMenu4 + '\n</ul>' + synRightMenu5.split('</a></li>\n</ul>')[1]
+//synRightMenu3 = synRightMenu3.split('Staff List</a></li>\n')[0] + 'Staff List</a></li>\n' + synRightMenu3.split('Staff List</a></li>\n')[1].split('My Profile</a></li>\n')[1]
+synRightMenu5 = '<div class="menu8"><ul>' + synMenuStart+ synHTML9[0] + 'Account/Clan</li>' + synHTML7[1].split('"header">Your Clan</li>')[1] + synHTML9[1]
+synRightMenu5 = synRightMenu5.split('</a></li>\n</ul>')[0] + '</a></li>' + /*synRightMenu4 +*/ '\n</ul>' + synRightMenu5.split('</a></li>\n</ul>')[1]
 synRightMenu5 = synRightMenu5.split("\n\n")
 synRightMenu = synRightMenu1 + "\n\n" + synRightMenu2 + "\n\n" + synRightMenu3 + "\n\n" + synRightMenu5
 
